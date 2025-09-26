4a6de1791b38b82b7c789a68d0f5616d761d4023
趙惟倫 <bluebat@member.fsf.org>
2025-09-26 15:19:21 +0800

    update prefer-choice.sh

--- prefer-choice.sh
+++ prefer-choice.sh
@@ -1,20 +1,21 @@
 #!/usr/bin/bash
 # Preferred Choices Arrangement
-# MIT license, 20250923~20250925
+# MIT license, 20250923~20250926
 # Wei-Lun Chao <bluebat@member.fsf.org>
 
 # INPUT.txt sample: (lines between COMMENTBLOCKs)
 <<COMMENTBLOCK
 # choice_code choice_name male_max female_min
-A Choice-A 2 1
-B Choice-B 2 1
-C Choice-C 2 1
+A Choice-A 3 1
+B Choice-B 3 1
+C Choice-C 4
 #..Z
 # person_number person_name person_gender 1st_prefer 2nd_prefer 3rd_prefer ..9th
 1 Person_1 M A B C
 2 Person_2 M B C
 3 Person_3 F B * *
 4 Person_4 F C A *
+5 Person_5 M A C B
 #..99
 COMMENTBLOCK
 
@@ -53,7 +54,7 @@ while read -a Data; do
   case "${Data[0]}" in
     [A-Z])
       Choice[${Data[0]},name]="${Data[1]}"
-      Choice[${Data[0]},male_max]="${Data[2]}"
+      test -n "${Data[2]}" && Choice[${Data[0]},male_max]=${Data[2]} || Choice[${Data[0]},male_max]=0
       Choice[${Data[0]},female_min]="${Data[3]}"
       test "${Data[0]}" \> "$Choice_Max" && Choice_Max="${Data[0]}"
       ;;
@@ -78,28 +79,48 @@ done < $Data_File
 for pm in letter star; do
   for pf in {1..9}; do
     for cc in {A..Z}; do
-      if test $((${Choice[$cc,female_min]}+${Choice[$cc,male_max]})) -gt 0; then
+      if test -z "${Choice[$cc,female_min]}"; then
+        Person_Gender="MF"
+        Gender_Total=${Choice[$cc,male_max]}
+      else
+        Person_Gender="M F"
+        Gender_Total=$((${Choice[$cc,female_min]}+${Choice[$cc,male_max]}))
+      fi
+      if test $Gender_Total -gt 0; then
         Male_Count=0
         Female_Count=0
         test $pm = letter && Prefer_Value=$cc || Prefer_Value="*"
         for pn in {1..99}; do
           if test "${Person[$pn,$pf]}" = "$Prefer_Value" -a "${Arrange[$pn,$cc]}" != Y; then
-            test "${Person[$pn,gender]}" = M && let Male_Count+=1 || let Female_Count+=1
+            if test "$Person_Gender" = "MF"; then
+              let Male_Count+=1
+            else
+              test "${Person[$pn,gender]}" = M && let Male_Count+=1 || let Female_Count+=1
+            fi
           fi
           test $pn -eq $Person_Max && break
         done
-        for pg in M F; do
-          test $pg = M && Person_Count=$Male_Count || Person_Count=$Female_Count
-          test $pg = M && Gender_Max=${Choice[$cc,male_max]} || Gender_Max=$((${Choice[$cc,female_min]}+${Choice[$cc,male_max]}))
+        for pg in $Person_Gender; do
+          if test -z "${pg##*M*}"; then
+            Person_Count=$Male_Count
+            Gender_Max=${Choice[$cc,male_max]}
+          else
+            Person_Count=$Female_Count
+            Gender_Max=$((${Choice[$cc,female_min]}+${Choice[$cc,male_max]}))
+          fi
           if test $(($Person_Count*$Gender_Max)) -eq 0; then
             true
           elif test $Person_Count -le $Gender_Max; then
             for pn in {1..99}; do
-              if test "${Person[$pn,$pf]}" = "$Prefer_Value" -a "${Person[$pn,gender]}" = $pg -a "${Arrange[$pn,$cc]}" != Y; then
+              if test "${Person[$pn,$pf]}" = "$Prefer_Value" -a "${Arrange[$pn,$cc]}" != Y -a -z "${pg##*${Person[$pn,gender]}*}"; then
                 Arrange[$pn,$cc]=Y
                 let Person[$pn,luck]+=1
-                if test $pg = F -a "${Choice[$cc,female_min]}" -gt 0; then
-                  let Choice[$cc,female_min]-=1
+                if test -n "${pg##*M*}"; then
+                  if test "${Choice[$cc,female_min]}" -gt 0; then
+                    let Choice[$cc,female_min]-=1
+                  else
+                    let Choice[$cc,male_max]-=1
+                  fi
                 else
                   let Choice[$cc,male_max]-=1
                 fi
@@ -109,16 +130,20 @@ for pm in letter star; do
           else
             while test $Gender_Max -gt 0; do
               for pn in {1..99}; do
-                if test "${Person[$pn,$pf]}" = "$Prefer_Value" -a "${Person[$pn,gender]}" = $pg -a "${Arrange[$pn,$cc]}" != Y -a $Gender_Max -gt 0; then
+                if test "${Person[$pn,$pf]}" = "$Prefer_Value" -a "${Arrange[$pn,$cc]}" != Y -a -z "${pg##*${Person[$pn,gender]}*}" -a $Gender_Max -gt 0; then
                   if test $RANDOM -lt $((32768*$Gender_Max/$Person_Count/(1+${Person[$pn,luck]}))); then
                     Arrange[$pn,$cc]=Y
                     let Person[$pn,luck]+=1
-                    if test $pg = F -a "${Choice[$cc,female_min]}" -gt 0; then
-                      let Choice[$cc,female_min]-=1
+                    if test -n "${pg##*M*}"; then
+                      if test "${Choice[$cc,female_min]}" -gt 0; then
+                        let Choice[$cc,female_min]-=1
+                      else
+                        let Choice[$cc,male_max]-=1
+                      fi
                     else
                       let Choice[$cc,male_max]-=1
                     fi
-                    let Person_Count-=1 Gender_Max-=1
+                    let Gender_Max-=1 Person_Count-=1
                   fi
                 fi
                 test $pn -eq $Person_Max && break
